8c174d93c4448fee5105024bb33d48da6f370620
Tcadle434 <thomascadle434@gmail.com>
2022-01-11 21:49:19 -0700

    mp4 to mov

--- src/containers/homepage/topSection.jsx
+++ src/containers/homepage/topSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Element } from "react-scroll";
-import BackgroundImg from "../../assets/pictures/compressback.mp4";
+import BackgroundImg from "../../assets/pictures/compressback.mov";
 import { Navbar } from "../../components/navbar";
 // import AWS from 'aws-sdk';
 
