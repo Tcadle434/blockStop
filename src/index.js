c6a3917cca9df30a8c3315842b61cad78674a109
Tcadle434 <thomascadle434@gmail.com>
2022-04-15 21:06:47 -0600

    fixes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
 
 
 ReactDOM.render(
+  <HttpsRedirect>
   <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </HttpsRedirect>,
   document.getElementById('root')
 );
 
